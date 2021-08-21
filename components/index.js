efd66b48cec3f058874e30fdc50bb136baf9fbcd
Myles Pasetsky <mp3blue99@gmail.com>
2021-08-21 00:30:39 -0400

    Create index of imports for all components

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as ConferenceTypes } from "./ConferenceTypes";
+export { default as Layout } from "./Layout";
+export { default as NamedList } from "./NamedList";
+export { default as NamedObject } from "./NamedObject";
+export { default as NewsPosts } from "./NewsPosts";
+export { default as NewsPost } from "./NewsPost";
