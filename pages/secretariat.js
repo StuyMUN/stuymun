2bb79bf8d54d07cff7c106f694052e7c27de50be
Myles Pasetsky <mp3blue99@gmail.com>
2021-09-03 21:54:56 -0400

    Add secretariat data and pill-based page

--- pages/secretariat.js
+++ pages/secretariat.js
@@ -1,23 +1,119 @@
-import { NamedList } from '../old/components';
 import { Site } from '../lib/data';
+import { Pill } from '../components';
+import Image from 'next/image';
 
-export default function SecretariatPage({ groups }) {
+function LowerMember({ person }) {
+    let size = 20;
+
+    let imageSize = `${size-2}px`;
+    /* eslint-disable-next-line @next/next/no-img-element*/
+    const PfpImage = <img src={person.pfp} width={imageSize} height={imageSize } alt="pfp" />;
+
+    let textSize = `${size}px`;
+    return <>
+        {person.pfp && PfpImage} <span style={{fontSize: textSize}}>{person.name}</span>
+    </>;
+}
+
+function Member({ person }) {
+    /* eslint-disable-next-line @next/next/no-img-element*/
+    const PfpImage = <img src={person.pfp} width="200px" height="200px" alt="pfp" />;
+
+    return <Pill>
+        <table>
+            <tr>
+                <td>{person.pfp && PfpImage}</td>
+                <td>
+                    <span>{person.name}</span>
+                </td>
+            </tr>
+        </table>
+    </Pill>;
+}
+
+function Position({ rank, position, people }) {
+
+    function getUpperMembers() {
+        return people.map((person, i) =>
+            <div key={i}>
+                <Member person={person} />;
+            </div>
+        );
+    }
+
+    function getLowerMembers() {
+        return <Pill>
+            {people.map((person, i) =>
+                <div key={i}>
+                    <LowerMember person={person} />
+                </div>
+            )}
+        </Pill>;
+    }
+
+    function getMembers() {
+        if (rank === 'Upper Secretariat') {
+            return getUpperMembers();
+        }
+        return getLowerMembers();
+    }
+
+    return <>
+        <header className="resources text-center">
+            <h3>{position}</h3>
+        </header>
+
+        {getMembers()}
+    </>;
+}
+
+function Rank({ rank, data }) {
+
+    function getPositions() {
+        return data.map(({ position, people }, i) => {
+            return <div key={i++}>
+                <Position
+                    rank={rank}
+                    position={position}
+                    people={people}
+                />
+            </div>;
+        })
+    }
+
+    return <>
+        <header className="resources text-center">
+            <h2>{rank}</h2>
+        </header>
+
+        {getPositions()}
+
+    </>;
+}
+
+export default function SecretariatPage({ sec }) {
+
     return <div>
-        <h2>Our team!</h2>
-        {groups.map((group, i) =>
-            <NamedList
-                key={i}
-                title={group.position}
-                list={group.people}
-            />
-        )}
+        <header className="resources text-center">
+            <h1>Meet the Team</h1>
+        </header>
+
+        {sec.map((rank, data, i) => {
+            return <div key={i}>
+                <Rank rank={rank} data={data} />
+            </div>;
+        })}
+
     </div>;
 }
 
 export async function getStaticProps() {
+
+    // let sec = await Site.getData('secretariat')
+
     return {
         props: {
-            groups: await Site.getData('secretariat')
+            sec: await Site.getData('secretariat')
         }
     };
 }
