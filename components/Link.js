61250daea103f098f40597288d60db49a5e8dad2
Myles Pasetsky <mp3blue99@gmail.com>
2021-09-03 21:51:37 -0400

    Update imports in components

--- components/Link.js
+++ components/Link.js
@@ -1,8 +1,6 @@
 import NextLink from 'next/link';
 
-export { default as NextLink } from 'next/link';
-
-export function Link({ children, href, passHref=false }) {
+export default function Link({ children, href, passHref=false }) {
     return <NextLink href={href} passHref={passHref}>
         <a>{children}</a>
     </NextLink>
--- components/index.js
+++ components/index.js
@@ -1,10 +1,11 @@
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 
-export {
-    default as Link,
-    default as NextLink
-} from './Link';
+export { default as Link } from './Link';
+export { default as NextLink } from 'next/link';
 
 export { default as NewsFeed } from './News';
 export { default as ConferenceFeed } from './Conferences';
+
+export { default as Pill } from './Pill';
+export { Post } from './Post';
