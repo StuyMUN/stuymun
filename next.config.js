0135dbb5ab3e8a39da74a58c60377439c8c9a519
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-15 20:59:25 -0400

    added basepath and assetprefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const debug = process.env.NODE_ENV !== "production";
 const path = require('path');
 
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    basePath:  !debug ? '/stuymun-fork' : '',
+    assetPrefix: !debug ? '/stuymun-fork' : '',
+    images: {
+      loader: "imgix",
+      path: "",
+    },
+    trailingSlash: true,
     reactStrictMode: true,
 }
