ab514083af450d2fe87dfc1f093cc4cdb7e9aece
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-19 15:42:23 -0400

    Fixed typo

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -21,7 +21,7 @@ export default function ResourcesPage() {
     return <>
 
         <Head>
-            <title>Resurces | StuyMUN</title>    
+            <title>Resources | StuyMUN</title>    
         </Head>
 
         <header className="text-center">
