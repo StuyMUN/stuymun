ce72821758af9d9c5059971f1300b679c8216521
ewong30 <78169577+ewong30@users.noreply.github.com>
2022-03-31 20:37:16 +0000

    Deploying to gh-pages from @ StuyMUN/stuymun@121bf846dfc9971c0f2a14cd4679e6972f2001c8 🚀

---
+++ _next/static/fauKUspZiPrrY64LuNml4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fconferences","\u002F","\u002Fnews","\u002Fsecretariat","\u002Fconference\u002F[name]","\u002Fconference\u002F[name]\u002F[slug]","\u002Fnews\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
