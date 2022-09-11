291ede0ce5db753e217b80fb19188ad482bc7316
AndySok <andrey.sokolov123@gmail.com>
2022-09-11 14:22:37 -0400

    small schedule fix + miniMUNC2021 renamed

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,10 +1,10 @@
 export default function Schedule({ }) {
     return <div>
         <h3 className='h2-title post-title'>Schedule</h3>
-        <b>8:45 - 9:15</b> Check-in<br/>
-        <b>9:15 - 9:50</b> Opening Ceremonies<br/>
-        <b>9:50 - 11:15 PM</b> Workshops<br/>
-        <b>11:20 - 1:10 PM</b> Committee Session I<br/>
+        <b>8:45 AM - 9:15 AM</b> Check-in<br/>
+        <b>9:15 AM - 9:50 AM</b> Opening Ceremonies<br/>
+        <b>9:50 AM - 11:15 PM</b> Workshops<br/>
+        <b>11:20 AM - 1:10 PM</b> Committee Session I<br/>
         <b>1:15 PM - 2:15 PM</b> Lunch Break Outdoors<br/>
         <b>2:15 PM - 5:15 PM</b> Committee Session II<br/>
         <b>5:20 PM - 5:45 PM</b> Closing Ceremonies
